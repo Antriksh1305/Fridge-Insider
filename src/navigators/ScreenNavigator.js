f7b0c9f8d9a2f0d7f6512cf69096e69d6c902655
Antriksh Gupta <Antrikshgupta01@gmail.com>
2023-09-10 18:14:13 +0530

    updates

--- src/navigators/ScreenNavigator.js
+++ src/navigators/ScreenNavigator.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import Index from '../screens/GetStarted/index';
-// import * as SplashScreen from 'expo-splash-screen';
-// import { useFonts } from 'expo-font';
 import AuthenticationPage from '../screens/Authentication/AuthenticationPage';
 import Location from '../screens/Location/Location';
 import ProvideLocation from '../screens/Location/ProvideLocation';
 import Main_home from '../screens/Homepage/Main_home';
 
 export default function ScreenNavigator() {
-      const [screen, changeScreen] = useState('AuthenticationPage');
+      const [screen, changeScreen] = useState('GetStarted');
 
       const screenComponents = {
             GetStarted: <Index changeScreen={changeScreen} />,
@@ -17,6 +15,5 @@ export default function ScreenNavigator() {
             ProvideLocation: <ProvideLocation changeScreen={changeScreen} />,
             MainHome: <Main_home changeScreen={changeScreen} />,
       };
-
       return screenComponents[screen];
 }
